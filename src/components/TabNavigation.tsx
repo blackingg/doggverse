3ffa83d31e663ad97189ec30cb5aeec231fc11ba
blackingg <odetundemubarak04@gmail.com>
2025-10-07 19:55:22 +0100

    fix: adjust z-index of TabNavigation component for better layering

--- src/components/TabNavigation.tsx
+++ src/components/TabNavigation.tsx
@@ -20,7 +20,7 @@ export const TabNavigation: React.FC<{
   ];
 
   return (
-    <div className="fixed bottom-0 left-0 right-0 bg-[#1c1c1e] border-t border-gray-800 z-50">
+    <div className="fixed bottom-0 left-0 right-0 bg-[#1c1c1e] border-t border-gray-800 z-40">
       <div className="flex justify-around items-center h-16 max-w-7xl mx-auto">
         {tabs.map((tab) => {
           const Icon = tab.icon;
